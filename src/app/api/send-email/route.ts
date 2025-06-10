14e39c45cca1dd34bce4b9cdadd1691e2646ec03
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2025-06-10 22:07:21 +0700

    feat(api): add nodemailer package in the api

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -1,3 +1,16 @@
+import { NextRequest } from "next/server";
+import nodemailer from 'nodemailer';
+
 export async function POST(request: NextRequest) {
-    
+    const body = await request.json();
+
+    const {emails, subject, message} = body;
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.GMAIL_USER,
+            pass: process.env.GMAIL_APP_PASS
+        }
+    })
 }
